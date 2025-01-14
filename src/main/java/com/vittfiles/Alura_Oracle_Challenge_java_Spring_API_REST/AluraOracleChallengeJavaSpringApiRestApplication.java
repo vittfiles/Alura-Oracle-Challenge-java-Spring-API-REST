9b0dafba0518ced34f4e2a0e7f2b4746321237ea
vittfiles <vittfiles@gmail.com>
2025-01-14 11:48:13 -0300

    first commit

---
+++ src/main/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/AluraOracleChallengeJavaSpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraOracleChallengeJavaSpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraOracleChallengeJavaSpringApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/AluraOracleChallengeJavaSpringApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraOracleChallengeJavaSpringApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
