ee9823f10ef4bfbaf58514f8ee5c728cd63f8435
vittfiles <vittfiles@gmail.com>
2025-01-16 19:07:10 -0300

    se modifico el endpoint de usuarios

--- src/main/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/controller/UserController.java
+++ src/main/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/usuarios")
 public class UserController {
 
     @Autowired
