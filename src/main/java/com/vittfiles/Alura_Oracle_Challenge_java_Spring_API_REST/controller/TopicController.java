1fb25ab376b688c5e59e3d7c3b504fa8b2320f36
vittfiles <vittfiles@gmail.com>
2025-01-17 15:25:34 -0300

    Se agrego la paginación a la lista de tópicos.

--- src/main/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/controller/TopicController.java
+++ src/main/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring_API_REST/controller/TopicController.java
@@ -1,6 +1,5 @@
 package com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.controller;
 
-import com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.domain.CustomValidationException;
 import com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.domain.course.CourseRepository;
 import com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.domain.topic.*;
 import com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.domain.topic.create_validators.CreateTopicValidator;
@@ -8,8 +7,11 @@ import com.vittfiles.Alura_Oracle_Challenge_java_Spring_API_REST.domain.user.Use
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +37,8 @@ public class TopicController {
     private UpdateTopicService updateTopicService;
 
     @GetMapping
-    public ResponseEntity<List<TopicDTO>> getTopics(){
-        var topics = topicRepository.findAll().stream()
-                .map(TopicDTO::new).toList();
+    public ResponseEntity<Page<TopicDTO>> getTopics(@PageableDefault(sort = "createdAt", direction = Sort.Direction.ASC) Pageable pagination){
+        var topics = topicRepository.findAll(pagination).map(TopicDTO::new);
         return ResponseEntity.ok(topics);
     }
 
